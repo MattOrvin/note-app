2e37a761b681485aad36797d8e4246a880aa3c4c
MattOrvin <matthew.orvin@gmail.com>
2020-02-13 11:38:30 -0600

    practicing hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js'
+import SideBar from './SideBar.js'
+import TextField from './TextField.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SideBar />
+      <TextField />
     </div>
   );
 }
---
+++ src/TextField.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function TextField(){
+
+    const [userText, setUserText] = useState("user text")
+    const [count, setCount] = useState(0)
+
+    return(
+        <div>
+            <h1>Text Field</h1>
+            <textarea onChange={() => setUserText}/>
+            <h3>{userText}</h3>
+            <p>You clicked the count button {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Increment
+            </button>
+            <br />
+            <button onClick={() => setCount(count - 1)}>
+                Decrement
+            </button>
+        </div>
+    )
+}
+
+export default TextField
